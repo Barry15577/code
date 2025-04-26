d437e5e0504810c192aabdfb65200c6563d4c50d
Barry15577 <jl15577@nyu.edu>
2025-04-26 12:09:11 +0800

    Update oss.vue

--- front/src/views/modules/oss/oss.vue
+++ front/src/views/modules/oss/oss.vue
@@ -2,9 +2,9 @@
   <div class="mod-oss">
     <el-form :inline="true" :model="dataForm">
       <el-form-item>
-        <el-button type="primary" @click="configHandle()">云存储配置</el-button>
-        <el-button type="primary" @click="uploadHandle()">上传文件</el-button>
-        <el-button type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量Delete</el-button>
+       <el-button type="primary" @click="configHandle()">Cloud storage configuration</el-button>
+<el-button type="primary" @click="uploadHandle()">Upload file</el-button>
+<el-button type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">Batch Delete</el-button>
       </el-form-item>
     </el-form>
     <el-table
@@ -30,21 +30,21 @@
         prop="url"
         header-align="center"
         align="center"
-        label="URL地址">
+       label="URL address">
       </el-table-column>
       <el-table-column
         prop="createDate"
         header-align="center"
         align="center"
         width="180"
-        label="创建时间">
+        label="Creation time">
       </el-table-column>
       <el-table-column
         fixed="right"
         header-align="center"
         align="center"
         width="150"
-        label="操作">
+        label="Action">
         <template slot-scope="scope">
           <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">Delete</el-button>
         </template>
@@ -59,9 +59,9 @@
       :total="totalPage"
       layout="total, sizes, prev, pager, next, jumper">
     </el-pagination>
-    <!-- 弹窗, 云存储配置 -->
-    <config v-if="configVisible" ref="config"></config>
-    <!-- 弹窗, 上传文件 -->
+   <!-- Pop-up window, cloud storage configuration -->
+<config v-if="configVisible" ref="config"></config>
+<!-- Pop-up window, upload file -->
     <upload v-if="uploadVisible" ref="upload" @refreshDataList="getDataList"></upload>
   </div>
 </template>
@@ -91,7 +91,7 @@
       this.getDataList()
     },
     methods: {
-      // 获取数据列表
+     // Get the data list
       getDataList () {
         this.dataListLoading = true
         this.$http({
@@ -112,44 +112,44 @@
           this.dataListLoading = false
         })
       },
-      // 每页数
-      sizeChangeHandle (val) {
-        this.pageSize = val
-        this.pageIndex = 1
-        this.getDataList()
-      },
-      // 当前页
-      currentChangeHandle (val) {
-        this.pageIndex = val
-        this.getDataList()
-      },
-      // 多选
-      selectionChangeHandle (val) {
-        this.dataListSelections = val
-      },
-      // 云存储配置
-      configHandle () {
-        this.configVisible = true
-        this.$nextTick(() => {
-          this.$refs.config.init()
-        })
-      },
-      // 上传文件
-      uploadHandle () {
-        this.uploadVisible = true
-        this.$nextTick(() => {
-          this.$refs.upload.init()
-        })
+    // Pages per page
+sizeChangeHandle (val) {
+this.pageSize = val
+this.pageIndex = 1
+this.getDataList()
+},
+// Current page
+currentChangeHandle (val) {
+this.pageIndex = val
+this.getDataList()
+},
+// Multiple selections
+selectionChangeHandle (val) {
+this.dataListSelections = val
+},
+// Cloud storage configuration
+configHandle () {
+this.configVisible = true
+this.$nextTick(() => {
+this.$refs.config.init()
+})
+},
+// Upload files
+uploadHandle () {
+this.uploadVisible = true
+this.$nextTick(() => {
+this.$refs.upload.init()
+})
       },
       // Delete
       deleteHandle (id) {
         var ids = id ? [id] : this.dataListSelections.map(item => {
           return item.id
         })
-        this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? 'Delete' : '批量Delete'}]操作?`, '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
+        this.$confirm(`Are you sure you want to perform the [${id ? 'Delete' : 'Batch Delete'}] operation on [id=${ids.join(',')}]?`, 'Prompt', {
+confirmButtonText: 'Confirm',
+cancelButtonText: 'Cancel',
+type: 'warning'
         }).then(() => {
           this.$http({
             url: this.$http.adornUrl('/sys/oss/delete'),
@@ -158,7 +158,7 @@
           }).then(({data}) => {
             if (data && data.code === 0) {
               this.$message({
-                message: '操作成功',
+              message: 'Operation successful',
                 type: 'success',
                 duration: 1500,
                 onClose: () => {
