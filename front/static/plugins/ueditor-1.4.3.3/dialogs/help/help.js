0b523c43550eb44c4e910e6fb450d70a3af25266
Barry15577 <jl15577@nyu.edu>
2025-04-26 12:29:12 +0800

    Update help.js

--- front/static/plugins/ueditor-1.4.3.3/dialogs/help/help.js
+++ front/static/plugins/ueditor-1.4.3.3/dialogs/help/help.js
@@ -1,23 +1,11 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: xuheng
- * Date: 12-9-26
- * Time: 下午1:06
- * To change this template use File | Settings | File Templates.
- */
-/**
- * tab点击处理事件
- * @param tabHeads
- * @param tabBodys
- * @param obj
- */
+
 function clickHandler( tabHeads,tabBodys,obj ) {
-    //head样式更改
-    for ( var k = 0, len = tabHeads.length; k < len; k++ ) {
-        tabHeads[k].className = "";
-    }
-    obj.className = "focus";
-    //body显隐
+   //head style change
+for ( var k = 0, len = tabHeads.length; k < len; k++ ) {
+tabHeads[k].className = "";
+}
+obj.className = "focus";
+//body visibility
     var tabSrc = obj.getAttribute( "tabSrc" );
     for ( var j = 0, length = tabBodys.length; j < length; j++ ) {
         var body = tabBodys[j],
@@ -35,9 +23,9 @@ function clickHandler( tabHeads,tabBodys,obj ) {
 }
 
 /**
- * TAB切换
- * @param tabParentId  tab的父节点ID或者对象本身
- */
+* TAB switching
+* @param tabParentId The parent node ID of the tab or the object itself
+*/
 function switchTab( tabParentId ) {
     var tabElements = $G( tabParentId ).children,
         tabHeads = tabElements[0].children,
