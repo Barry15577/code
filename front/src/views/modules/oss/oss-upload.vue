3fd3a7fb2200d3e8c545e93f248fb318b98249ba
Barry15577 <jl15577@nyu.edu>
2025-04-26 12:07:47 +0800

    Update oss-upload.vue

--- front/src/views/modules/oss/oss-upload.vue
+++ front/src/views/modules/oss/oss-upload.vue
@@ -1,6 +1,6 @@
 <template>
   <el-dialog
-    title="上传文件"
+    title="Upload file"
     :close-on-click-modal="false"
     @close="closeHandle"
     :visible.sync="visible">
@@ -13,8 +13,8 @@
       :file-list="fileList"
       style="text-align: center;">
       <i class="el-icon-upload"></i>
-      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-      <div class="el-upload__tip" slot="tip">只支持jpg、png、gif格式的图片！</div>
+      <div class="el-upload__text">Drag the file here, or <em>click to upload</em></div>
+<div class="el-upload__tip" slot="tip">Only supports jpg, png, and gif format images! </div>
     </el-upload>
   </el-dialog>
 </template>
@@ -35,24 +35,24 @@
         this.url = this.$http.adornUrl(`/sys/oss/upload?token=${this.$cookie.get('token')}`)
         this.visible = true
       },
-      // 上传之前
-      beforeUploadHandle (file) {
-        if (file.type !== 'image/jpg' && file.type !== 'image/jpeg' && file.type !== 'image/png' && file.type !== 'image/gif') {
-          this.$message.error('只支持jpg、png、gif格式的图片！')
-          return false
+     // Before uploading
+beforeUploadHandle (file) {
+if (file.type !== 'image/jpg' && file.type !== 'image/jpeg' && file.type !== 'image/png' && file.type !== 'image/gif') {
+this.$message.error('Only support jpg, png, gif format images!')
+return false
         }
         this.num++
       },
-      // 上传成功
-      successHandle (response, file, fileList) {
-        this.fileList = fileList
-        this.successNum++
-        if (response && response.code === 0) {
-          if (this.num === this.successNum) {
-            this.$confirm('操作成功, 是否继续操作?', '提示', {
-              confirmButtonText: '确定',
-              cancelButtonText: '取消',
-              type: 'warning'
+      // Upload successful
+successHandle (response, file, fileList) {
+this.fileList = fileList
+this.successNum++
+if (response && response.code === 0) {
+if (this.num === this.successNum) {
+this.$confirm('Operation successful, do you want to continue?', 'Prompt', {
+confirmButtonText: 'Confirm',
+cancelButtonText: 'Cancel',
+type: 'warning'
             }).catch(() => {
               this.visible = false
             })
@@ -61,7 +61,7 @@
           this.$message.error(response.msg)
         }
       },
-      // 弹窗关闭时
+     // When the popup window is closed
       closeHandle () {
         this.fileList = []
         this.$emit('refreshDataList')
