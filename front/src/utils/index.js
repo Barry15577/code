5d7fa4daa7894cca30cb8a2f652dde36f3e8ee9c
Barry15577 <jl15577@nyu.edu>
2025-04-26 11:43:25 +0800

    Update index.js

--- front/src/utils/index.js
+++ front/src/utils/index.js
@@ -3,8 +3,8 @@ import router from '@/router'
 import store from '@/store'
 
 /**
- * 获取uuid
- */
+* Get uuid
+*/
 export function getUUID () {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
     return (c === 'x' ? (Math.random() * 16 | 0) : ('r&0x3' | '0x8')).toString(16)
@@ -12,19 +12,19 @@ export function getUUID () {
 }
 
 /**
- * 是否有权限
- * @param {*} key
- */
+* Is there permission?
+* @param {*} key
+*/
 export function isAuth (key) {
-  return JSON.parse(sessionStorage.getItem('permissions') || '[]').indexOf(key) !== -1 || false
+return JSON.parse(sessionStorage.getItem('permissions') || '[]').indexOf(key) !== -1 || false
 }
 
 /**
- * 树形数据转换
- * @param {*} data
- * @param {*} id
- * @param {*} pid
- */
+* Tree data conversion
+* @param {*} data
+* @param {*} id
+* @param {*} pid
+*/
 export function treeDataTranslate (data, id = 'id', pid = 'parentId') {
   var res = []
   var temp = {}
@@ -49,8 +49,8 @@ export function treeDataTranslate (data, id = 'id', pid = 'parentId') {
 }
 
 /**
- * 清除登录信息
- */
+* Clear login information
+*/
 export function clearLoginInfo () {
   Vue.cookie.delete('token')
   store.commit('resetStore')
