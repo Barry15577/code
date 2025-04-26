e62b9bc840012970a88592ed9636fd5dfc81a43b
Barry15577 <jl15577@nyu.edu>
2025-04-26 11:48:35 +0800

    Update ueditor.vue

--- front/src/views/demo/ueditor.vue
+++ front/src/views/demo/ueditor.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="mod-demo-ueditor">
     <el-alert
-      title="提示："
+      title="Hint："
       type="warning"
       :closable="false">
-      <div slot-scope="description">
-        <p class="el-alert__description">1. 此Demo只提供UEditor官方使用文档，入门部署和体验功能。具体使用请参考：http://fex.baidu.com/ueditor/</p>
-        <p class="el-alert__description">2. 浏览器控制台报错“请求后台配置项http错误，上传功能将不能正常使用！”，此错需要后台提供上传接口方法（赋值给serverUrl属性）</p>
-      </div>
     </el-alert>
 
     <script :id="ueId" class="ueditor-box" type="text/plain" style="width: 100%; height: 260px;">hello world!</script>
-    
-    <!-- 获取内容 -->
-    <p><el-button @click="getContent()">获得内容</el-button></p>
-    <el-dialog
-      title="内容"
+    <!-- Get content -->
+<p><el-button @click="getContent()">Get content</el-button></p>
+<el-dialog
+title="Content"
       :visible.sync="dialogVisible"
       :append-to-body="true">
       {{ ueContent }}
@@ -39,7 +34,7 @@
     },
     mounted () {
       this.ue = ueditor.getEditor(this.ueId, {
-        // serverUrl: '', // 服务器统一请求接口路径
+        // serverUrl: '', //Server unified request interface path
         zIndex: 3000
       })
     },
