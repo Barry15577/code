2768b57b15be2ba1af7d6b227eb48e8357c79820
Barry15577 <jl15577@nyu.edu>
2025-04-26 11:43:40 +0800

    Update validate.js

--- front/src/utils/validate.js
+++ front/src/utils/validate.js
@@ -1,31 +1,31 @@
 /**
- * 邮箱
- * @param {*} s
- */
+* Email
+* @param {*} s
+*/
 export function isEmail (s) {
-  return /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(s)
+return /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(s)
 }
 
 /**
- * 手机号码
- * @param {*} s
- */
+* Mobile phone number
+* @param {*} s
+*/
 export function isMobile (s) {
-  return /^1[0-9]{10}$/.test(s)
+return /^1[0-9]{10}$/.test(s)
 }
 
 /**
- * 电话号码
- * @param {*} s
- */
+* Phone number
+* @param {*} s
+*/
 export function isPhone (s) {
-  return /^([0-9]{3,4}-)?[0-9]{7,8}$/.test(s)
+return /^([0-9]{3,4}-)?[0-9]{7,8}$/.test(s)
 }
 
 /**
- * URL地址
- * @param {*} s
- */
-export function isURL (s) {
-  return /^http[s]?:\/\/.*/.test(s)
+* URL address
+* @param {*} s 
+*/
+export function isURL (s) { 
+return /^http[s]?:\/\/.*/.test(s)
 }
